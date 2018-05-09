174d72b1c959503a041db76b0cf23469ac3e0c20
martin6724 <martin.michelleann@gmail.com>
2018-05-09 16:29:33 -0400

    mostly working model on my branch.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,12 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+
+
+
+
+
+#HAD TO ADD THESE TO DISPLAY API. NEVER FORGET.
+Rails.application.config.assets.precompile += %w( organ_systems.js )
+Rails.application.config.assets.precompile += %w( organ_systems.scss.css )
