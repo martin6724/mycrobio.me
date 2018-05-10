72c12bbac19ce2370af1da5b279e4ee892b54ac3
adinelson <adimrd@gmail.com>
2018-05-10 15:21:41 -0400

    adding views

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   get 'organ_systems/index'
   get 'organ_systems/show'
   get 'organ_systems/create'
+  get 'home/index'
+  root 'home#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
