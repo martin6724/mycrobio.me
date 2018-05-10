caa3180714a78fe7f4aadc1ff260ebd0c8e8c19d
martin6724 <martin.michelleann@gmail.com>
2018-05-10 14:26:07 -0400

    BOOYA SUCKA

--- app/assets/javascripts/organ_systems.js
+++ app/assets/javascripts/organ_systems.js
@@ -3,24 +3,28 @@ $(document).on('turbolinks:load', function () {
 // Initialize API
 var human = new HumanAPI("embedded-human");
 
-$("#nasopharynx").on("click", function() {
+$("#nasopharynx").on("click", function(e) {
+  e.preventDefault();
   human.send("camera.set", { objectId: "human_05_male_respiratory_system-pharynx_ID", animate: true });
 });
 
-$("#respiratory").on("click", function() {
+$("#respiratory").on("click", function(e) {
+  e.preventDefault();
   human.send("camera.set", { objectId: "human_05_male_respiratory_system-respiratory_system_ID", animate: true });
 });
 
-$("#stomach").on("click", function() {
+$("#stomach").on("click", function(e) {
+  e.preventDefault();
   human.send("camera.set", { objectId: "human_05_male_digestive_system-stomach_ID", animate: true });
 });
 
-$("#smallintestine").on("click", function() {
+$("#smallintestine").on("click", function(e) {
+  e.preventDefault();
   human.send("camera.set", { objectId: "human_05_male_digestive_system-small_intestines_ID", animate: true });
-  event.preventDefault();
 });
 
-$("#largeintestine").on("click", function() {
+$("#largeintestine").on("click", function(e) {
+  e.preventDefault();
   human.send("camera.set", { objectId: "human_05_male_digestive_system-colon_ID", animate: true });
 });
 })
