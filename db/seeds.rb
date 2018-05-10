bb7728b2f6cbe2663dba3f5de15955c3766ba0c4
Tereza Oprea <terezacamelia@gmail.com>
2018-05-10 16:45:24 -0400

    seeds working

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,92 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-require 'nokogiri'
 require 'json'
 
-html = File.read "db/bugs.html"
-dom = Nokogiri::HTML html
-names = []
-data = dom.css("tr").map do |row|
-	names << row.text.strip.split("\n")[0]
+html = File.read('db/bugs.html')
+require 'nokogiri'
+
+dom = Nokogiri::HTML(html)
+values = {
+    'nasopharynx' => [
+        "mouth",
+        "nasopharynx",
+        "nose",
+        "oropharynx",
+        "pharynx",
+        "pharynx[citation needed]",
+        "saliva",
+        "teeth: dental plaque",
+        "throat",
+    ],
+    'respiratory' => [
+        "lung", 
+        "respiratory epithelium",
+        "sputum",
+        "upper respiratory tract",
+    ],
+    'stomach' => [
+        "stomach",
+    ],
+    'small intestine' => [
+        "gi tract",
+        "intestines",
+        "small and large intestine",
+        "small intestine",
+        "small intestine (ileon)",
+    ],
+    'large intestine' => [
+        "anus channel",
+        "cecum",
+        "gi tract (known probiotic)",
+        "large intestine",
+        "rectus",
+    ],
+    'urethra' => [
+        "anterior urethra",
+        "t",
+    ],
+    'genitalia' => [
+        "external genitalia",
+        "external genitalia,",
+        "perineum",
+    ],
+    'skin' => [
+        "general distribution",
+    ],
+}
+lookup = Hash[values.values.zip(values.keys)]
+
+base_data = dom.css('tr').map do |row|
+    row.text.strip
 end
+    .select do |item|
+        item.include? "\n"
+    end
+    .map do |item|
+        item.split("\n")
+    end
+    .reject do |item|
+        item.first.include? "name" or item.first == "v"
+    end
+    .reject do |bug, location|
+        location.include?("1983")
+    end
+    .map do |bug, locations|
+        [bug.downcase, locations.split(', ').map{|item| item.downcase}]
+    end
+    .reject do |bug, locations|
+        locations.include? "feces"
+    end
+    .map do |bug, locations|
+        locations.product([bug])
+    end.flatten(1).map do |location, bug|
+        # {flora: {bug: bug, organ_system: location}}
+        lookup.map do |locations, body_spot|
+            if locations.include?(location)
+                {wiki: location, tereza: body_spot, bug: bug}
+            end
+        end.compact
+    end.flatten(1)
 
-names = names.slice(2..-6)
-
-names.each do |x|
-	Organism.create(name: x)
-end
-
-organ_systems = JSON.parse(File.read('db/organ_systems.json'))
-organ_systems.each do |y|
-	OrganSystem.create(name: y)
+base_data.each do |item|
+    puts item
 end
 
