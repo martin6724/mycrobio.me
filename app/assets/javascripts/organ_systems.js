f1412a47138eeb139b7712bcab87807d2a9e3c11
martin6724 <martin.michelleann@gmail.com>
2018-05-11 10:45:13 -0400

    debugged jquery scroll feature

--- app/assets/javascripts/organ_systems.js
+++ app/assets/javascripts/organ_systems.js
@@ -1,8 +1,16 @@
 
 $(document).on('turbolinks:load', function () {
+
+//
+$('html, body').css({
+      overflow: 'auto',
+      height: 'auto'
+  });
+
 // Initialize API
 var human = new HumanAPI("embedded-human");
 
+// male 3d model
 $("#nasopharynx").on("click", function(e) {
   e.preventDefault();
   human.send("camera.set", { objectId: "human_05_male_respiratory_system-pharynx_ID", animate: true });
@@ -27,6 +35,39 @@ $("#largeintestine").on("click", function(e) {
   e.preventDefault();
   human.send("camera.set", { objectId: "human_05_male_digestive_system-colon_ID", animate: true });
 });
+
+
+
+
+// female 3d model
+var fhuman = new HumanAPI("fembedded-human");
+
+$("#fnasopharynx").on("click", function(e) {
+  e.preventDefault();
+  fhuman.send("camera.set", { objectId: "human_05_male_respiratory_system-pharynx_ID", animate: true });
+});
+
+$("#frespiratory").on("click", function(e) {
+  e.preventDefault();
+  fhuman.send("camera.set", { objectId: "human_05_male_respiratory_system-respiratory_system_ID", animate: true });
+});
+
+$("#fstomach").on("click", function(e) {
+  e.preventDefault();
+  fhuman.send("camera.set", { objectId: "human_05_male_digestive_system-stomach_ID", animate: true });
+});
+
+$("#fsmallintestine").on("click", function(e) {
+  e.preventDefault();
+  fhuman.send("camera.set", { objectId: "human_05_male_digestive_system-small_intestines_ID", animate: true });
+});
+
+$("#flargeintestine").on("click", function(e) {
+  e.preventDefault();
+  fhuman.send("camera.set", { objectId: "human_05_male_digestive_system-colon_ID", animate: true });
+});
+
+
 
 
 })
