b23c61ae943e84603285977959709fda669ac46d
adinelson <adimrd@gmail.com>
2018-05-12 12:14:28 -0400

    desgin changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   get 'organ_systems/index'
   get 'organ_systems/show'
   get 'organ_systems/create'
-  get 'home/index'
+  get 'home/about' => 'home#about', as: 'about'
+  get 'home/index' => 'home#index', as: 'home'
   root 'home#index'
-  get 'organ_systems/female_index' => 'organ_systems#female_index'
-  get 'organ_systems/male_index' => 'organ_systems#male_index'
+  get 'organ_systems/female_index' => 'organ_systems#female_index' , as: 'female'
+  get 'organ_systems/male_index' => 'organ_systems#male_index', as: 'male'
 
   get 'organ_systems/nasopharynx' => 'organ_systems#modal1_trigger', as: 'fnasopharynx'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
