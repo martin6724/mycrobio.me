4612dbb57fd4a959f2fcc71fca510adb41d8de21
martin6724 <martin.michelleann@gmail.com>
2018-05-12 11:38:12 -0400

    changes to 3d model and model skeleton beginnings

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
   get 'organ_systems/create'
   get 'home/index'
   root 'home#index'
-  get 'organ_systems/female_index'
-  get 'organ_systems/male_index'
+  get 'organ_systems/female_index' => 'organ_systems#female_index'
+  get 'organ_systems/male_index' => 'organ_systems#male_index'
 
+  get 'organ_systems/nasopharynx' => 'organ_systems#modal1_trigger', as: 'fnasopharynx'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
