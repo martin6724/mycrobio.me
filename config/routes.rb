4b9dbb8a6de94687a4f615a39421bf59875512fa
martin6724 <martin.michelleann@gmail.com>
2018-05-17 09:35:59 -0400

    floras controller, for active record query variables

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   get 'home/about' => 'home#about', as: 'about'
   get 'home/index' => 'home#index', as: 'home'
   root 'home#index'
+
   get 'organ_systems/female_index' => 'organ_systems#female_index' , as: 'female'
   get 'organ_systems/male_index' => 'organ_systems#male_index', as: 'male'
-
   get 'organ_systems/nasopharynx' => 'organ_systems#nasopharynx_trigger', as: 'nasopharynx'
   get 'organ_systems/respiratory' => 'organ_systems#respiratory_trigger', as: 'respiratory'
   get 'organ_systems/stomach' => 'organ_systems#stomach_trigger', as: 'stomach'
@@ -17,5 +17,12 @@ Rails.application.routes.draw do
   get 'organ_systems/female_genitalia' => 'organ_systems#genitalia_trigger', as: 'female_genitalia'
   get 'organ_systems/male_genitalia' => 'organ_systems#genitalia_trigger', as: 'male_genitalia'
   get 'organ_systems/skin' => 'organ_systems#skin_trigger', as: 'skin'
+
+
+# the floras partial is a page in the floras controller designed to organize all of the
+# active record results for flora modals. Pending.
+  # get 'floras_partial' => ''
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
