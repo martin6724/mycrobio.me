d8bb29653d2a6da19f5ef167d6d3f27834a5ec3b
Tereza Oprea <terezacamelia@gmail.com>
2018-05-18 12:12:17 -0400

    moved search logic to view, not controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,16 +2,15 @@ class HomeController < ApplicationController
 	def search
   		@results = Organism.where("name LIKE ?", "%#{params[:term]}%")
   		
-	  		if @results.empty?
-	  			puts "No organisms were found. Try again!" #msg in terminal
-	  			flash[:error] = "No organisms were found. Try again!"
-      			redirect_to root_path
-	  		else 
-		  		@results.each do |result|
-		    		puts result.name #in terminal
-		    		flash[:success] = "Search successful!"
-      				#redirect_to result_path
-		    	end
-  			end
+	  		# if @results.empty? #moved this to navbar view
+	  		# 	flash[:error] = "No organisms were found. Try again!"
+     #  			redirect_to root_path
+	  		# else 
+		  	# 	@results.each do |result|
+		   #  		puts result.name #in terminal
+		   #  		flash[:success] = "Search successful!"
+     #  				#redirect_to result_path
+		   #  	end
+  			# end
 	end
 end
