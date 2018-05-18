7026b135705cf87f95f11d34c2418d087e27c618
Tereza Oprea <terezacamelia@gmail.com>
2018-05-18 11:33:27 -0400

    add logic for flash message for search feature

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,16 @@ class HomeController < ApplicationController
 	def search
   		@results = Organism.where("name LIKE ?", "%#{params[:term]}%")
   		
-	  		if @results.nil?
-	  			puts "No organisms were found. Try again!"
+	  		if @results.empty?
+	  			puts "No organisms were found. Try again!" #msg in terminal
+	  			flash[:error] = "No organisms were found. Try again!"
+      			redirect_to root_path
 	  		else 
 		  		@results.each do |result|
-		    		link_to result.name, result
+		    		# link_to result.name, result
+		    		puts result.name
+		    		flash[:success] = "Search successful!"
+      				#redirect_to result_path
 		    	end
   			end
 	end
