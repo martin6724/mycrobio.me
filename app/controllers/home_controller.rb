46c18a66aaf78dab852575f894411175940e1094
Tereza Oprea <terezacamelia@gmail.com>
2018-05-18 13:55:44 -0400

    view for search results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,10 +2,7 @@ class HomeController < ApplicationController
 	def search
   		@results = Organism.where("name LIKE ?", "%#{params[:term]}%")
 	  		unless @results.empty?
-		  		@results.each do |result|
-		    		puts result.name #in terminal
-		    	end
-		    	render 'kitty'
+		    	render 'search_results'
 	  		else
 	  			puts "Nothing found."
 	  			redirect_to root_path
