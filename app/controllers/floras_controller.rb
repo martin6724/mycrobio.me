4c4d358b87b56a54f8925dc162ac35bac0ec5af9
martin6724 <martin.michelleann@gmail.com>
2018-05-18 15:33:00 -0400

    debugged flora show pages

--- app/controllers/floras_controller.rb
+++ app/controllers/floras_controller.rb
@@ -2,12 +2,11 @@ class FlorasController < ApplicationController
 
 def index
   @floras = Flora.all
-  @antibiotics = Antibiotics.all
 end
 
 def show
   @singleton_floras = [Flora.find(params[:id]), ]
-  @antibiotic_efficacies = [Antibiotic.find(params[:id]), ]
+
 end
 
 end
