82ace18f4cd466188e62da190fb1de7fbb58d72c
martin6724 <martin.michelleann@gmail.com>
2018-05-17 21:04:27 -0400

    The names of all organisms finally display in their respective organ_system modals! party

--- app/controllers/organ_systems_controller.rb
+++ app/controllers/organ_systems_controller.rb
@@ -8,37 +8,61 @@ class OrganSystemsController < ApplicationController
   def create
   end
 
+  def female_index
+  end
+
   def nasopharynx_trigger
+    nasopharynx = OrganSystem.find_by(name: 'nasopharynx')
+  @nasofos =  Flora.where(organ_system: nasopharynx)
+    .map{ |flora| flora.organism.name }
   end
 
   def respiratory_trigger
+    respiratory = OrganSystem.find_by(name: 'respiratory')
+  @respifos =  Flora.where(organ_system: respiratory)
+    .map{ |flora| flora.organism.name }
   end
 
   def stomach_trigger
+    stomach = OrganSystem.find_by(name: 'stomach')
+  @stomafos =  Flora.where(organ_system: stomach)
+    .map{ |flora| flora.organism.name }
   end
 
   def smallintestine_trigger
+    smallintestine = OrganSystem.find_by(name: 'small intestine')
+  @sifos =  Flora.where(organ_system: smallintestine)
+    .map{ |flora| flora.organism.name }
   end
 
   def largeintestine_trigger
-    large_intestine = OrganSystem.find_by(name: 'large intestine')
-    @lifos = Flora.where(organ_system: large_intestine)
+    largeintestine = OrganSystem.find_by(name: 'large intestine')
+    @lifos = Flora.where(organ_system: largeintestine)
       .map{ |flora| flora.organism.name }
-    if @lifos.empty?
-      raise RuntimeError.new '@lifos are empty'
-    end
   end
 
   def urinary_trigger
+    urethra = OrganSystem.find_by(name: 'urethra')
+  @urifos =  Flora.where(organ_system: urethra)
+    .map{ |flora| flora.organism.name }
   end
 
   def female_genitalia_trigger
+    female_genitalia = OrganSystem.find_by(name: 'genitalia')
+    @femgenit = Flora.where(organ_system: genitalia)
+      .map{ |flora| flora.organism.name }
   end
 
   def male_genitalia_trigger
+    male_genitalia = OrganSystem.find_by(name: 'genitalia')
+    @genit = Flora.where(organ_system: genitalia)
+      .map{ |flora| flora.organism.name }
   end
 
   def skin_trigger
+    skin = OrganSystem.find_by(name: 'skin')
+  @skifos =  Flora.where(organ_system: skin)
+    .map{ |flora| flora.organism.name }
   end
 
 end
